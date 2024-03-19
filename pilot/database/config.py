e595b9cbf303eb2e895454d53c3aec2a3b637844
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:32:34 -0500

    comment

--- pilot/database/config.py
+++ pilot/database/config.py
@@ -1,8 +1,15 @@
+# Import the `os` module to interact with the operating system
 import os
 
+# Set the default database type to SQLite
 DATABASE_TYPE = os.getenv("DATABASE_TYPE", "sqlite")
+# Get the name of the database from the environment variable `DB_NAME`
 DB_NAME = os.getenv("DB_NAME")
+# Get the host of the database from the environment variable `DB_HOST`
 DB_HOST = os.getenv("DB_HOST")
+# Get the port of the database from the environment variable `DB_PORT`
 DB_PORT = os.getenv("DB_PORT")
+# Get the user of the database from the environment variable `DB_USER`
 DB_USER = os.getenv("DB_USER")
+# Get the password of the database from the environment variable `DB_PASSWORD`
 DB_PASSWORD = os.getenv("DB_PASSWORD")
