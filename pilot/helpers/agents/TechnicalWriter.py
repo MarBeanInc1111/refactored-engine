5e9ac285fee312db2d360b388ba9f83346f206e7
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:58:10 -0500

    improve

--- pilot/helpers/agents/TechnicalWriter.py
+++ pilot/helpers/agents/TechnicalWriter.py
@@ -1,21 +1,25 @@
+import colorama  # Importing colorama for console output coloring
+from pathlib import Path  # Using Path from pathlib for file handling
+from typing import Dict, List  # Adding type hints
 from const.function_calls import GET_DOCUMENTATION_FILE  # Importing the constant for documentation file
 from helpers.AgentConvo import AgentConvo  # Importing AgentConvo helper class
 from helpers.Agent import Agent  # Importing Agent base class
 from utils.files import count_lines_of_code  # Importing function to count lines of code
-from utils.style import color_green_bold, color_green  # Importing coloring functions for console output
+
+colorama.init(autoreset=True)  # Initializing colorama
 
 class TechnicalWriter(Agent):  # Inheriting from Agent base class
-    def __init__(self, project):
+    def __init__(self, project: Path):
         super().__init__('technical_writer', project)  # Initializing the Agent base class
         self.save_dev_steps = True  # Attribute to save development steps
 
-    def document_project(self, percent):
-        files = self.project.get_all_coded_files()  # Getting all coded files from the project
-        print(f'{color_green_bold("CONGRATULATIONS!!!")}', category='success')  # Printing congratulations message
-        print(f'You reached {color_green(str(percent) + "%")} of your project generation!\n\n')  # Printing project progress
+    def document_project(self, percent: float):
+        coded_files = self.project.glob('**/*.py')  # Getting all coded files from the project
+        print(f'{colorama.Style.BRIGHT + colorama.Fore.GREEN}CONGRATULATIONS!!!{colorama.Style.RESET_ALL}', category='success')  # Printing congratulations message
+        print(f'You reached {colorama.Fore.GREEN + str(percent) + "%" + colorama.Style.RESET_ALL} of your project generation!\n\n')  # Printing project progress
         print('For now, you have created:\n')  # Informing about the created files and lines of code
-        print(f'{color_green(len(files))} files\n')
-        print(f'{color_green(count_lines_of_code(files))} lines of code\n\n')
+        print(f'{colorama.Fore.GREEN + str(len(list(coded_files))) + " files" + colorama.Style.RESET_ALL}\n')
+        print(f'{colorama.Fore.GREEN + str(count_lines_of_code(list(coded_files))) + " lines of code" + colorama.Style.RESET_ALL}\n\n')
         print('Before continuing, GPT Pilot will create some documentation for the project...\n')  # Creating documentation
         print('', type='verbose', category='agent:tech-writer')  # Printing verbose message
         self.create_license()  # Creating a license
@@ -27,23 +31,23 @@ class TechnicalWriter(Agent):  # Inheriting from Agent base class
         pass  # Placeholder for creating a license file
 
     def create_readme(self):
-        print(color_green('Creating README.md'))  # Printing message for creating README.md
+        print(colorama.Fore.GREEN + 'Creating README.md' + colorama.Style.RESET_ALL)  # Printing message for creating README.md
         convo = AgentConvo(self)  # Initializing AgentConvo helper class
 
         # Sending a message to the LLM (Language Model) to create the README.md file
         llm_response = convo.send_message('documentation/create_readme.prompt', {
-            "name": self.project.args['name'],
+            "name": self.project.name,
             "app_type": self.project.args['app_type'],
             "app_summary": self.project.project_description,
             "user_stories": self.project.user_stories,
             "user_tasks": self.project.user_tasks,
             "directory_tree": self.project.get_directory_tree(True),
-            "files": self.project.get_all_coded_files(),
+            "files": list(coded_files),
             "previous_features": self.project.previous_features,
             "current_feature": self.project.current_feature,
         }, GET_DOCUMENTATION_FILE)
 
-        self.project.save_file(llm_response)  # Saving the generated README.md file
+        self.project.save_file(llm_response, 'README.md')  # Saving the generated README.md file
         return convo  # Returning the AgentConvo instance for potential further use
 
     def create_api_documentation(self):
