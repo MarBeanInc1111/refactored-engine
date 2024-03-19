4d59d79c4f15c206c20cb4ba1e85b7656277b1fa
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:57:52 -0500

    improve

--- pilot/database/models/development_planning.py
+++ pilot/database/models/development_planning.py
@@ -2,20 +2,15 @@
 from database.config import DATABASE_TYPE
 from database.models.components.progress_step import ProgressStep
 from database.models.components.sqlite_middlewares import JSONField
+from playhouse.postgres_ext import BinaryJSONField
 
-# In case the database is PostgreSQL, use the PostgreSQL JSONField
 if DATABASE_TYPE == 'postgres':
-    # Import BinaryJSONField from Playhouse's PostgreSQL extensions
-    from playhouse.postgres_ext import BinaryJSONField
-
-    class DevelopmentPlanning(ProgressStep):
-        development_plan = BinaryJSONField()
-
-# If the database is not PostgreSQL (e.g., SQLite), use the custom JSONField
+    field_type = BinaryJSONField
 else:
-    class DevelopmentPlanning(ProgressStep):
-        development_plan = JSONField()  # Custom JSON field for SQLite
+    field_type = JSONField
 
-# Define the table name for the DevelopmentPlanning model
-class Meta:
-    table_name = 'development_planning'
+class DevelopmentPlanning(ProgressStep):
+    development_plan = field_type()
+
+    class Meta:
+        table_name = 'development_planning'
