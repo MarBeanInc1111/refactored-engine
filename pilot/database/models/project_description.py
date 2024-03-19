c25692caceaa6b1ba253951a99ee25fa29a24031
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:32:38 -0500

    comment

--- pilot/database/models/project_description.py
+++ pilot/database/models/project_description.py
@@ -1,10 +1,22 @@
 from peewee import TextField
 from database.models.components.progress_step import ProgressStep
 
-
 class ProjectDescription(ProgressStep):
+    """
+    A model class that represents a project description in the database.
+    Inherits from ProgressStep model class.
+    """
     prompt = TextField()
+    """
+    A text field that stores the prompt for the project description.
+    """
     summary = TextField()
+    """
+    A text field that stores the summary of the project description.
+    """
 
     class Meta:
+        """
+        Meta class that specifies the name of the table in the database.
+        """
         table_name = 'project_description'
