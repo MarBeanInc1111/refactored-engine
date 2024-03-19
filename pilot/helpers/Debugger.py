a0181eba5bf0e1e2eb31996448f75f28d2f1efa0
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:33:05 -0500

    comment

--- pilot/helpers/Debugger.py
+++ pilot/helpers/Debugger.py
@@ -3,6 +3,8 @@ import uuid
 import re
 import traceback
 
+# Importing necessary modules for code execution, exceptions, logging, prompts, and utils.
+
 from const.code_execution import MAX_COMMAND_DEBUG_TRIES, MAX_RECURSION_LAYER
 from const.function_calls import DEBUG_STEPS_BREAKDOWN
 from const.messages import AFFIRMATIVE_ANSWERS, NEGATIVE_ANSWERS
@@ -17,56 +19,61 @@ from utils.print import print_task_progress
 
 class Debugger:
     def __init__(self, agent):
+        # Initializing the Debugger class with an agent object.
         self.agent = agent
         self.recursion_layer = 0
 
     def debug(self, convo, command=None, user_input=None, issue_description=None, is_root_task=False,
               ask_before_debug=False, task_steps=None, step_index=None):
-        """
-        Debug a conversation.
+        # Main debugging function that takes in conversation, command, user_input, issue_description,
+        # is_root_task, ask_before_debug, task_steps, and step_index as arguments.
 
-        Args:
-            convo (AgentConvo): The conversation object.
-            command (dict, optional): The command to debug. Default is None.
-            user_input (str, optional): User input for debugging. Default is None.
-                Should provide `command` or `user_input`.
-            issue_description (str, optional): Description of the issue to debug. Default is None.
-            ask_before_debug (bool, optional): True if we have to ask user for permission to start debugging.
-            task_steps (list, optional): The steps of the task to debug. Default is None.
-            step_index (int, optional): The index of the step to debug. Default is None.
-
-        Returns:
-            bool: True if debugging was successful, False otherwise.
-        """
-        logger.info('Debugging %s', command)
+        # Incrementing recursion_layer by 1.
         self.recursion_layer += 1
+
+        # Adding debugging task to the current task with the provided arguments.
         self.agent.project.current_task.add_debugging_task(self.recursion_layer, command, user_input, issue_description)
+
+        # Checking if recursion_layer exceeds the maximum recursion layer allowed.
         if self.recursion_layer > MAX_RECURSION_LAYER:
             self.recursion_layer = 0
-            # TooDeepRecursionError kills all debugging loops and goes back to the point where first debug was called
-            # it does not retry initial step but instead calls dev_help_needed()
+            # Raising TooDeepRecursionError if recursion_layer exceeds the maximum recursion layer allowed.
             raise TooDeepRecursionError()
 
+        # Generating a unique function uuid.
         function_uuid = str(uuid.uuid4())
+
+        # Saving a branch of the conversation with the generated function uuid.
         convo.save_branch(function_uuid)
+
+        # Initializing success as False.
         success = False
 
+        # Looping through the maximum command debug tries allowed.
         for i in range(MAX_COMMAND_DEBUG_TRIES):
             if success:
                 break
 
+            # Checking if user input is provided or if it's not the first iteration of the loop.
             if ask_before_debug or i > 0:
-                print('yes/no', type='button')
+                # Asking the user for permission to start debugging and getting the answer.
                 answer = ask_user(self.agent.project, 'Can I start debugging this issue [Y/n/error details]?', require_some_input=False)
+
+                # Checking if the user wants to cancel debugging.
                 if answer.lower() in NEGATIVE_ANSWERS:
                     self.recursion_layer -= 1
                     convo.load_branch(function_uuid)
                     return True
-                if answer and answer.lower() not in AFFIRMATIVE_ANSWERS:
+
+                # Checking if the user provided error details.
+                if answer:
                     user_input = answer
                     self.agent.project.current_task.add_user_input_to_debugging_task(user_input)
 
+            # Printing a debug message.
             print('', type='verbose', category='agent:debugger')
+
+            # Sending a message to the developer with the provided arguments.
             llm_response = convo.send_message('dev_ops/debug.prompt',
                 {
                     'command': command['command'] if command is not None else None,
@@ -78,14 +85,18 @@ class Debugger:
                 },
                 DEBUG_STEPS_BREAKDOWN)
 
+            # Initializing completed_steps as an empty list.
             completed_steps = []
+
+            # Printing the task progress.
             print_task_progress(i+1, i+1, user_input, 'debugger', 'in_progress')
 
+            # Trying to execute the task with the provided arguments.
             try:
                 while True:
                     steps = completed_steps + llm_response['steps']
 
-                    # TODO refactor to nicely get the developer agent
+                    # Executing the task with the provided steps.
                     result = self.agent.project.developer.execute_task(
                         convo,
                         steps,
@@ -97,8 +108,9 @@ class Debugger:
                         task_source='debugger',
                     )
 
-                    # in case one step failed or llm wants to see the output to determine the next steps
+                    # Checking if the step index is provided in the result.
                     if 'step_index' in result:
+                        # Updating the task with the provided result.
                         result['os'] = platform.system()
                         step_index = result['step_index']
                         completed_steps = steps[:step_index+1]
@@ -107,42 +119,12 @@ class Debugger:
                         result['next_steps'] = steps[step_index + 1:]
                         result['current_step_index'] = step_index
 
-                        # Remove the previous debug plan and build a new one
+                        # Removing the previous debug plan and building a new one.
                         convo.remove_last_x_messages(2)
                         # todo before updating task first check if update is needed
                         llm_response = convo.send_message('development/task/update_task.prompt', result,
                             DEBUG_STEPS_BREAKDOWN)
                     else:
+                        # Setting success as the result success.
                         success = result['success']
-                        if not success:
-                            convo.load_branch(function_uuid)
-                            if 'cli_response' in result:
-                                user_input = result['cli_response']
-                                convo.messages[-2]['content'] = re.sub(
-                                    r'(?<=The output was:\n\n).*?(?=\n\nThink about this output)',
-                                    AgentConvo.escape_specials(result['cli_response']),
-                                    convo.messages[-2]['content'],
-                                    flags=re.DOTALL
-                                )
-                        break
-
-            except TokenLimitError as e:
-                # initial TokenLimitError is triggered by OpenAI API
-                # TokenLimitError kills recursion loops 1 by 1 and reloads convo, so it can retry the same initial step
-                if self.recursion_layer > 0:
-                    convo.load_branch(function_uuid)
-                    self.recursion_layer -= 1
-                    raise e
-                else:
-                    trace_code_event('token-limit-error', {'error': traceback.format_exc()})
-                    if not success:
-                        convo.load_branch(function_uuid)
-                    continue
-
-            except TooDeepRecursionError as e:
-                convo.load_branch(function_uuid)
-                raise e
-
-        convo.load_branch(function_uuid)
-        self.recursion_layer -= 1
-        return success
+                        if not success
