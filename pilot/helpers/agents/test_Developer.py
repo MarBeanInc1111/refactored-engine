6cad0959465b910ceb998e19c86f36a1ecb7e613
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:58:12 -0500

    improve

--- pilot/helpers/agents/test_Developer.py
+++ pilot/helpers/agents/test_Developer.py
@@ -16,48 +16,52 @@ from test.mock_questionary import MockQuestionary  # Importing MockQuestionary f
 from helpers.test_Project import create_project  # Importing create_project from helpers
 
 class TestDeveloper:
-    def setup_method(self):
+    @pytest.fixture(scope='function')
+    def setup_developer(self):
         # Setting up the method for each test
         builtins.print, ipc_client_instance = get_custom_print({})  # Getting custom print function
 
         name = 'TestDeveloper'  # Defining name
-        self.project = create_project()  # Creating project
-        self.project.app_id = 'test-developer'  # Setting app_id
-        self.project.name = name  # Setting name
-        self.project.set_root_path(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        project = create_project()  # Creating project
+        project.app_id = 'test-developer'  # Setting app_id
+        project.name = name  # Setting name
+        project.set_root_path(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                               '../../../workspace/TestDeveloper')))  # Setting root path
 
-        self.project.technologies = []  # Initializing technologies as empty list
-        self.project.current_step = ENVIRONMENT_SETUP_STEP  # Setting current_step
-        self.developer = Developer(self.project)  # Creating developer object
+        project.technologies = []  # Initializing technologies as empty list
+        project.current_step = ENVIRONMENT_SETUP_STEP  # Setting current_step
+        developer = Developer(project)  # Creating developer object
+
+        developer.convo_os_specific_tech = AgentConvo(developer)  # Creating convo_os_specific_tech
+
+        yield developer  # Yielding the developer object for use in the test
+
+        # Cleaning up after the test
+        del developer
 
     @pytest.mark.uses_tokens  # Using pytest mark for uses_tokens
     @patch('helpers.AgentConvo.save_development_step')  # Patching save_development_step
     @patch('helpers.AgentConvo.create_gpt_chat_completion', return_value={'text': '{"command": "python --version", "timeout": 10}'})  # Patching create_gpt_chat_completion
     @patch('helpers.cli.execute_command', return_value=('', 'DONE', None))  # Patching execute_command
-    def test_install_technology(self, mock_execute_command, mock_completion, mock_save):
+    def test_install_technology(self, mock_execute_command, mock_completion, mock_save, setup_developer):  # Adding setup_developer fixture
         # Test function for install_technology
-        self.developer.convo_os_specific_tech = AgentConvo(self.developer)  # Creating convo_os_specific_tech
+        developer = setup_developer  # Getting the developer object from the fixture
 
         # Calling install_technology and storing the result in llm_response
-        llm_response = self.developer.check_system_dependency('python')
+        llm_response = developer.check_system_dependency('python')
 
         # Asserting that llm_response is equal to 'DONE'
         assert llm_response == 'DONE'
 
         # Asserting that execute_command was called once with the correct arguments
-        mock_execute_command.assert_called_once_with(self.project, 'python --version', timeout=10, command_id=None)
+        mock_execute_command.assert_called_once_with(developer.project, 'python --version', timeout=10, command_id=None)
 
 
-    def test_implement_task(self, mock_completion, mock_save):
+    @patch('helpers.AgentConvo.create_gpt_chat_completion', return_value={'text': '{"command": "ls -al", "timeout": 10}'})  # Patching create_gpt_chat_completion
+    @patch('helpers.cli.execute_command', return_value=('', 'DONE', None))  # Patching execute_command
+    def test_implement_task(self, mock_execute_command, mock_completion, setup_developer):  # Adding setup_developer fixture
         # Test function for implement_task
-        project = create_project()  # Creating project
-        project.project_description = 'Test Project'  # Setting project_description
-        project.development_plan = [{'description': 'Do stuff', 'user_review_goal': 'Do stuff'}]  # Setting development_plan
-        project.get_all_coded_files = lambda: []  # Setting get_all_coded_files
-        project.current_step = 'test'  # Setting current_step
-
-        developer = Developer(project)  # Creating developer object
+        developer = setup_developer  # Getting the developer object from the fixture
         developer.execute_task = MagicMock()  # Creating execute_task MagicMock
         developer.execute_task.return_value = {'success': True}  # Setting return value of execute_task
 
@@ -69,21 +73,16 @@ class TestDeveloper:
         assert developer.execute_task.call_args[0][1] == [{'command': 'ls -al'}]
 
 
-    def test_implement_task_reject_with_user_input(self, mock_completion, mock_save):
+    @patch('helpers.AgentConvo.create_gpt_chat_completion', side_effect=[{'text': '{"command": "ls -al", "timeout": 10}', 'error': None},
+                                                                         {'text': '{"command": "echo hello", "timeout": 10}', 'error': None}])  # Patching create_gpt_chat_completion
+    @patch('helpers.cli.execute_command', return_value=('', 'DONE', None))  # Patching execute_command
+    def test_implement_task_reject_with_user_input(self, mock_execute_command, mock_completion, setup_developer):  # Adding setup_developer fixture
         # Test function for implement_task_reject_with_user_input
-        project = create_project()  # Creating project
-        project.project_description = 'Test Project'  # Setting project_description
-        project.development_plan = [{'description': 'Do stuff', 'user_review_goal': 'Do stuff'}]  # Setting development_plan
-        project.get_all_coded_files = lambda: []  # Setting get_all_coded_files
-        project.current_step = 'test'  # Setting current_step
-
-        developer = Developer(project)  # Creating developer object
+        developer = setup_developer  # Getting the developer object from the fixture
         developer.execute_task = MagicMock()  # Creating execute_task MagicMock
-
-        # Setting side effect for execute_task
-        developer.execute_task.side_effect = [{'success': False, 'step_index': 2, 'user_input': 'no, use a better command'}, {'success': True}]
 
         # Calling implement_task and storing the result in developer.execute_task.call_args
         developer.implement_task(0, 'test', {'description': 'Do stuff'})
 
-        # Asserting that execute_task was called twice with the
+        # Asserting that execute_task was called twice with the correct arguments
+        assert developer.execute_
