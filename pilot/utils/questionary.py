9a3932e56a75462d83e74b756f3b27f126eb54fb
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:33:04 -0500

    comment

--- pilot/utils/questionary.py
+++ pilot/utils/questionary.py
@@ -1,12 +1,22 @@
-import platform
-import questionary
-import sys
-from database.database import save_user_input
-from utils.style import style_config
-from utils.print import remove_ansi_codes
-
+import platform  # Importing platform to get the current platform information
+import questionary  # Importing questionary to create interactive command-line prompts
+import sys  # Importing sys for accessing system-specific parameters and functions
+from database.database import save_user_input  # Importing save_user_input function to save user inputs
+from utils.style import style_config  # Importing style_config to get the preferred style
+from utils.print import remove_ansi_codes  # Importing remove_ansi_codes to remove ANSI escape codes
 
 def styled_select(*args, **kwargs):
+    """
+    A function to create a styled select prompt using questionary.
+    This function allows users to select one option from a list of choices.
+
+    Args:
+    *args: Variable length argument list.
+    **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+    The selected option from the list of choices.
+    """
     kwargs["style"] = style_config.get_style()
     # TODO add saving and loading of user input
     return questionary.select(*args, **kwargs).unsafe_ask()  # .ask() is included here
@@ -14,10 +24,18 @@ def styled_select(*args, **kwargs):
 
 def styled_text(project, question, ignore_user_input_count=False, style=None, hint=None):
     """
-    Ask the user a question and return the answer.
+    A function to ask the user a question and return the answer.
+    This function handles colorama and questionary incompatibility issues and saves user inputs.
 
-    NOTE: Use ask_user() instead of this function!
-    Use styled_text() only if you don't want to trigger project.finish_loading()!
+    Args:
+    project: The project object.
+    question: The question to be asked to the user.
+    ignore_user_input_count: A boolean to ignore the user input count.
+    style: The style to be used for the question.
+    hint: The hint to be displayed for the question.
+
+    Returns:
+    The answer provided by the user.
     """
     if not ignore_user_input_count:
         project.user_inputs_count += 1
@@ -39,26 +57,36 @@ def styled_text(project, question, ignore_user_input_count=False, style=None, hi
 
 
 def get_user_feedback():
+    """
+    A function to get user feedback about the application.
+
+    Returns:
+    The feedback provided by the user.
+    """
     return questionary.text('How did GPT Pilot do? Were you able to create any app that works? '
                             'Please write any feedback you have or just press ENTER to exit: ',
                             style=style_config.get_style()).unsafe_ask()
 
 
 def ask_user_to_store_init_prompt():
+    """
+    A function to ask the user if they want to store the initial app prompt.
+
+    Returns:
+    The user's response to storing the initial app prompt.
+    """
     return questionary.text('We would appreciate if you let us store your initial app prompt. '
                             'If you are OK with that, please just press ENTER',
                             style=style_config.get_style()).unsafe_ask()
 
 
 def flush_input():
-    """Flush the input buffer, discarding all that's in the buffer."""
+    """
+    A function to flush the input buffer, discarding all that's in the buffer.
+    """
     try:
         if platform.system() == 'Windows':
             import msvcrt
             while msvcrt.kbhit():
                 msvcrt.getch()
-        else:
-            import termios
-            termios.tcflush(sys.stdin, termios.TCIOFLUSH)
-    except (ImportError, OSError):
-        pass
+        else
