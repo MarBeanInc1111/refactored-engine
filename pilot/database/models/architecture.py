048ad07a730f8e2372e45111f0029093a6b98e5e
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:57:52 -0500

    improve

--- pilot/database/models/architecture.py
+++ pilot/database/models/architecture.py
@@ -1,18 +1,17 @@
 # Import necessary modules
-from peewee import  # Peewee ORM for working with databases
+from peewee import *  # Peewee ORM for working with databases
 from database.config import DATABASE_TYPE  # Database type configuration
 from database.models.components.progress_step import ProgressStep  # Base progress step model
-from database.models.components.sqlite_middlewares import JSONField  # Custom JSON field for SQLite
-from playhouse.postgres_ext import BinaryJSONField  # Binary JSON field for PostgreSQL
 
+if DATABASE_TYPE == 'postgres':
+    db_field = BinaryJSONField()
+else:
+    db_field = JSONField()
 
 # Subclass ProgressStep for the Architecture model
 class Architecture(ProgressStep):
     # Conditionally define the architecture field based on the database type
-    if DATABASE_TYPE == 'postgres':
-        architecture = BinaryJSONField()  # Use BinaryJSONField for PostgreSQL
-    else:
-        architecture = JSONField()  # Use JSONField for SQLite
+    architecture = db_field
 
     # Define the table name for the Architecture model
     class Meta:
