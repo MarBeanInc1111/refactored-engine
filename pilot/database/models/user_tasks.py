958c6b107b0ab414eb5d32dbff2f47dfc185f0d2
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-19 16:57:52 -0500

    improve

--- pilot/database/models/user_tasks.py
+++ pilot/database/models/user_tasks.py
@@ -6,9 +6,10 @@ from playhouse.postgres_ext import BinaryJSONField
 
 class UserTasks(ProgressStep):
     if DATABASE_TYPE == 'postgres':
-        user_tasks = BinaryJSONField()
+        user_tasks = BinaryJSONField(index=True)
     else:
-        user_tasks = JSONField()  # Custom JSON field for SQLite
+        user_tasks = JSONField(index=True)  # Custom JSON field for SQLite
 
     class Meta:
         table_name = 'user_tasks'
+
