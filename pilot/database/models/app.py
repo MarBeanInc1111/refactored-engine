cb9f365906391977f87eb32fa824b85f05e94b5e
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-22 08:19:49 -0500

    improve

--- pilot/database/models/app.py
+++ pilot/database/models/app.py
@@ -3,9 +3,14 @@ from peewee import ForeignKeyField, CharField
 from database.models.components.base_models import BaseModel
 from database.models.user import User
 
+class AppStatus(BaseModel):
+    status = CharField()
+
+class AppType(BaseModel):
+    app_type = CharField()
 
 class App(BaseModel):
     user = ForeignKeyField(User, backref='apps')
-    app_type = CharField(null=True)
-    name = CharField(null=True)
-    status = CharField(null=True)
+    app_type = ForeignKeyField(AppType, backref='apps')
+    name = CharField()
+    status = ForeignKeyField(AppStatus, backref='apps')
