ef61cf4603477d27a6a3e56a4e1e405713314238
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-22 08:19:54 -0500

    improve

--- pilot/utils/test_files.py
+++ pilot/utils/test_files.py
@@ -2,7 +2,11 @@ import os
 from unittest.mock import patch
 from utils.files import setup_workspace
 
-# Mock function for os.makedirs to avoid creating directories during testing
 def mock_makedirs(*args, **kwargs):
     return
+
+@patch('utils.files.os.makedirs', mock_makedirs)
+def test_setup_workspace():
+    setup_workspace('test_workspace')
+    assert os.path.exists('test_workspace'), 'Workspace directory should have been created'
 
