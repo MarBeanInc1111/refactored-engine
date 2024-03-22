fefbadc931ec9deb2d36c00553bb3a21266da929
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-22 08:19:45 -0500

    improve

--- pilot/const/telemetry.py
+++ pilot/const/telemetry.py
@@ -1,3 +1,11 @@
-LARGE_REQUEST_THRESHOLD = 50000  # tokens
+# Constants representing various thresholds for request handling and task processing
+
+"""
+LARGE_REQUEST_THRESHOLD: The number of tokens that defines a large request.
+SLOW_REQUEST_THRESHOLD: The number of seconds that defines a slow request.
+LOOP_THRESHOLD: The number of iterations in a task that defines a loop.
+"""
+
+LARGE_REQUEST_THRESHOLD = 50_000  # tokens
 SLOW_REQUEST_THRESHOLD = 300  # seconds
 LOOP_THRESHOLD = 3  # number of iterations in task to be considered a loop
