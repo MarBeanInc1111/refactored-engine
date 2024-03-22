fe4c2cb6fc7579383982ba5b3e4cf8d701042abc
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-03-22 08:19:54 -0500

    improve

--- pilot/database/models/feature.py
+++ pilot/database/models/feature.py
@@ -1,21 +1,21 @@
-from peewee import ForeignKeyField, CharField, BooleanField, DateTimeField
+from peewee import ForeignKeyField, CharField, BooleanField, DateTimeField, JSONField
 from database.config import DATABASE_TYPE
 from database.models.components.base_models import BaseModel
 from database.models.app import App
 from database.models.development_steps import DevelopmentSteps
-from database.models.components.sqlite_middlewares import JSONField
-from playhouse.postgres_ext import BinaryJSONField
-
 
 class Feature(BaseModel):
-    app = ForeignKeyField(App, backref='feature', on_delete='CASCADE')
+    app = ForeignKeyField(App, backref='features', on_delete='CASCADE')
     summary = CharField()
 
     if DATABASE_TYPE == 'postgres':
-        messages = BinaryJSONField(null=True)
+        messages = JSONField(null=True)
     else:
         messages = JSONField(null=True)
 
-    previous_step = ForeignKeyField(DevelopmentSteps, column_name='previous_step')
+    previous_step = ForeignKeyField(DevelopmentSteps, column_name='previous_step_id')
     completed = BooleanField(default=False)
     completed_at = DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'features'
